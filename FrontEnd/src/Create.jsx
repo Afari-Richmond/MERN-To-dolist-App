81ff2be0238ee355e9d401e92c976c5f17f2cf45
richmondafari <afaririchmond1000@gmail.com>
2025-01-13 08:45:56 +0000

    Initial Commit

---
+++ FrontEnd/src/Create.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+const Create = () => {
+  const [task, setTask] = useState();
+
+  const handleAddTask = () => {
+    axios
+      .post("http://localhost:3001/add", { task: task })
+      .then((result) => console.log(result))
+      .catch((err) => console.log(err));
+  };
+
+  return (
+    <>
+      <div className="create_form">
+        <input
+          type="text"
+          name=""
+          id=""
+          placeholder="Enter a task here"
+          onChange={(e) => setTask(e.target.value)}
+        />
+        <button type="button" onClick={handleAddTask}>
+          {" "}
+          Add Task
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default Create;
